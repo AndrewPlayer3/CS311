cceacf37d7f330ea5e18212235de818e80bfd297
Andrew Player <andrewplayer3@gmail.com>
2020-09-04 12:43:24 -0800

    more cleaning and anal spacing

--- Project1/productorder.h
+++ Project1/productorder.h
@@ -22,25 +22,30 @@ private:
 public:
 
     //Constructors
+    
     ProductOrder();
-    ProductOrder(const ProductOrder& Product);
-    ProductOrder(ProductOrder&& Product);
-    ProductOrder(const std::string& name, const int& number);
-    ~ProductOrder();
+    ProductOrder(const ProductOrder&  Product);
+    ProductOrder(      ProductOrder&& Product);
+    ProductOrder(const std::string &  name, const int& number);
+   ~ProductOrder();
+
 
     //Members
+     
     std::string getName  () const;
     int         getNumber() const;
-    void        setName  (const std::string& name);
-    void        setNumber(const int& number);
+    void        setName  (  const std::string& name  );
+    void        setNumber(  const int&         number);
     bool        empty    () const;
     std::string toString () const;
     
+
     //Overloads
-    ProductOrder& operator= (const ProductOrder& Product);
+    
+    ProductOrder& operator= (const ProductOrder&  Product);
     ProductOrder& operator= (const ProductOrder&& Product);
-    bool          operator==(const ProductOrder& Product) const;
-    bool          operator!=(const ProductOrder& Product) const;
+    bool          operator==(const ProductOrder&  Product) const;
+    bool          operator!=(const ProductOrder&  Product) const;
     ProductOrder& operator++();
     ProductOrder  operator++(int);
     ProductOrder& operator--();
