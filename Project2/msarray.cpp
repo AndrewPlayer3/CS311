33d5e70cc250565d75925b55262b4e5584df561d
Andrew Player <andrewplayer3@gmail.com>
2020-09-14 13:12:19 -0800

    initial code

--- Project2/msarray.cpp
+++ Project2/msarray.cpp
@@ -9,54 +9,71 @@
 
 template<typename T>
 MSArray<T>::MSArray() {
-
+    _size = 8;
+    for(int i = 0; i < _size; i++) {
+        _data[i] = 0;
+    }
 }
 
 template<typename T>
 MSArray<T>::MSArray(const MSArray<T>& array) {
-
+    _size = array._size;
+    _data = array._data;
 }
 
 template<typename T>
 MSArray<T>::MSArray(const MSArray<T>&& array) {
-
+    _size = array._size;
+    _data = array._data;
 }
 
 template<typename T>
 MSArray<T>::MSArray(const std::size_t& size ) {
-
+    _size = size;
+    for(int i = 0; i < size; i++) {
+        _data[i] = 0;
+    }
 }
 
 template<typename T>
 MSArray<T>::MSArray(const std::size_t& size, const T& fill) {
-
+    _size = size;
+    for(int i = 0; i < size; i++) {
+        _data[i] = fill;
+    }
 }
 
 template<typename T>
-T* end() {
-
+T* MSArray<T>::end() {
+    return _data[_size - 1];
 }
 
 template<typename T>
-T* begin() {
-
+T* MSArray<T>::begin() {
+    return _data[0];
 }
 
-std::size_t size();
+template<typename T>
+std::size_t MSArray<T>::size() {
+    return _size;
+}
 
 template<typename T>
 MSArray<T>& MSArray<T>::operator=(const MSArray<T>&  array) {
-
+    _size = array._size;
+    _data = array._data;
 }
 
 template<typename T>
 MSArray<T>& MSArray<T>::operator=(const MSArray<T>&& array) {
-
+    _size = array._size;
+    _data = array._data;
 }
 
 template<typename T>
 T& MSArray<T>::operator[](const std::size_t& location) {
-
+    if(location > 0 && location < _size) return _data[location];
+    else std::cout << "ERROR HERE" << std::endl;
 }
 
 template<typename T>
@@ -86,5 +103,5 @@ bool MSArray<T>::operator==(const MSArray<T>& array) {
 
 template<typename T>
 bool MSArray<T>::operator!=(const MSArray<T>& array) {
-    
+
 }
--- Project2/msarray.h
+++ Project2/msarray.h
@@ -15,18 +15,19 @@ class MSArray {
 
 private:
     
-    T _data[];
+    T* _data;
+    std::size_t _size;
 
 public:
 
     typedef T value_type;
     typedef std::size_t size_type;
 
-    MSArray<T>();
-    MSArray<T>(const MSArray<T>&     array);
-    MSArray<T>(const MSArray<T>&&    array);
-    MSArray<T>(const std::size_t& size );
-    MSArray<T>(const std::size_t& size, const T& fill);
+    MSArray();
+    MSArray(const MSArray<T>& array);
+    MSArray(const MSArray<T>&& array);
+    MSArray(const std::size_t& size );
+    MSArray(const std::size_t& size, const T& fill);
 
     T* end();
     T* begin();
@@ -45,5 +46,104 @@ public:
     bool operator!=(const MSArray<T>& array);
 
 };
+
+template<typename T>
+MSArray<T>::MSArray() {
+    _size = 8;
+    for(int i = 0; i < _size; i++) {
+        _data[i] = 0;
+    }
+}
+
+template<typename T>
+MSArray<T>::MSArray(const MSArray<T>& array) {
+    _size = array._size;
+    _data = array._data;
+}
+
+template<typename T>
+MSArray<T>::MSArray(const MSArray<T>&& array) {
+    _size = array._size;
+    _data = array._data;
+}
+
+template<typename T>
+MSArray<T>::MSArray(const std::size_t& size ) {
+    _size = size;
+    for(int i = 0; i < size; i++) {
+        _data[i] = 0;
+    }
+}
+
+template<typename T>
+MSArray<T>::MSArray(const std::size_t& size, const T& fill) {
+    _size = size;
+    for(int i = 0; i < size; i++) {
+        _data[i] = fill;
+    }
+}
+
+template<typename T>
+T* MSArray<T>::end() {
+    return _data[_size - 1];
+}
+
+template<typename T>
+T* MSArray<T>::begin() {
+    return _data[0];
+}
+
+template<typename T>
+std::size_t MSArray<T>::size() {
+    return _size;
+}
+
+template<typename T>
+MSArray<T>& MSArray<T>::operator=(const MSArray<T>&  array) {
+    _size = array._size;
+    _data = array._data;
+}
+
+template<typename T>
+MSArray<T>& MSArray<T>::operator=(const MSArray<T>&& array) {
+    _size = array._size;
+    _data = array._data;
+}
+
+template<typename T>
+T& MSArray<T>::operator[](const std::size_t& location) {
+    if(location > 0 && location < _size) return _data[location];
+    else std::cout << "ERROR HERE" << std::endl;
+}
+
+template<typename T>
+bool MSArray<T>::operator< (const MSArray<T>& array) {
+
+}
+
+template<typename T>
+bool MSArray<T>::operator>(const MSArray<T>& array) {
+
+}
+
+template<typename T>
+bool MSArray<T>::operator<=(const MSArray<T>& array) {
+
+}
+
+template<typename T>
+bool MSArray<T>::operator>=(const MSArray<T>& array) {
+
+}
+
+template<typename T>
+bool MSArray<T>::operator==(const MSArray<T>& array) {
+
+}
+
+template<typename T>
+bool MSArray<T>::operator!=(const MSArray<T>& array) {
+
+}
 
 #endif
