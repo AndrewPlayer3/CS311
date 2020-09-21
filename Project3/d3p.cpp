608c29a3c2bdc622becca7c5dd90017a3f677aab
Andrew Player <andrewplayer3@gmail.com>
2020-09-20 16:45:04 -0800

    added source and header files

---
+++ Project3/d3p.cpp
@@ -0,0 +1,8 @@
+/*
+ * Andrew Player
+ * CS311
+ * 09/24/2020
+ * Source for Project 3 
+*/
+
+#include "d3p.h"
---
+++ Project3/d3p.h
@@ -0,0 +1,13 @@
+/*
+ * Andrew Player
+ * CS311
+ * 09/24/2020
+ * Header for Project 3 
+*/
+
+#ifndef D3P_H_
+#define D3P_H_
+
+#include <iostream>
+
+#endif
