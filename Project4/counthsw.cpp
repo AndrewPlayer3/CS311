81c2d7a9f14dbcb42b525f77a1c9564b78bcd970
Andrew Player <andrewplayer3@gmail.com>
2020-10-03 01:07:24 -0500

    added initial files for project 4

---
+++ Project4/counthsw.cpp
@@ -0,0 +1,1 @@
+#include "counthsw.h"
---
+++ Project4/counthsw.h
@@ -0,0 +1,15 @@
+/*
+ * Andrew Player
+ * CS311
+ * 10/03/2020
+ * Header for Project 4
+*/
+
+#ifndef COUNTHSW_H_
+#define COUNTHSW_H_
+
+#include <iostream>
+
+
+
+#endif
