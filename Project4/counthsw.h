4c17025ca1d6550f1e833b0d08bb2c603d052fc7
Andrew Player <andrewplayer3@gmail.com>
2020-10-03 01:28:40 -0500

    added counthsw prototype

--- Project4/counthsw.h
+++ Project4/counthsw.h
@@ -10,6 +10,9 @@
 
 #include <iostream>
 
-
+int countHSW(int dim_x, int dim_y,
+             int hole_x, int hole_y,
+             int start_x, int start_y,
+             int finish_x, int finish_y);
 
 #endif
