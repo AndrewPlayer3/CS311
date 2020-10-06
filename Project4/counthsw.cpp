7b9bc91ce6009b0aa8d4cb35432df3666a05200e
Andrew Player <andrewplayer3@gmail.com>
2020-10-06 18:30:47 -0500

    comments

--- Project4/counthsw.cpp
+++ Project4/counthsw.cpp
@@ -1,19 +1,27 @@
 #include "counthsw.h"
 
 int countHSW_recurse(std::vector<std::vector<int>> board, int cur_x, int cur_y, int finish_x, int finish_y, int squares_left) {   
+    
+    // Test for valid location
     if(cur_x < 0 || cur_y < 0
       || cur_x > board.size() - 1
       || cur_y > board[0].size() - 1
       || board[cur_x][cur_y] == 1) return 0;
     board[cur_x][cur_y] = 1;
+    
+    // Test for complete solution
     if(cur_x == finish_x && cur_y == finish_y
            && squares_left == 0) {
         return 1;
     }
+
+    // Temp variables to restore after recursive call
     int total = 0;
     int temp_x = cur_x;
     int temp_y = cur_y;
     int squares_temp = squares_left;
+    
+    // Go through every adjacent and diagonal place
     for(int i = -1; i < 2; i++) {
         for(int j = -1; j < 2; j++) {
             if(i == 0 && j == 0) continue;
