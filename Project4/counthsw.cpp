8fd76c3d9cc794814ad52f7d3cbaaa06bacd888e
Andrew Player <andrewplayer3@gmail.com>
2020-10-06 18:19:46 -0500

    implemented squares_left properly

--- Project4/counthsw.cpp
+++ Project4/counthsw.cpp
@@ -6,20 +6,14 @@ int countHSW_recurse(std::vector<std::vector<int>> board, int cur_x, int cur_y, 
       || cur_y > board[0].size() - 1
       || board[cur_x][cur_y] == 1) return 0;
     board[cur_x][cur_y] = 1;
-    if(squares_left > 0) squares_left--;
     if(cur_x == finish_x && cur_y == finish_y
            && squares_left == 0) {
-        bool is_zero = false;
-        for(std::vector<int> row : board) {
-            for(int place : row) {
-                if(place == 0) is_zero = true;
-            }
-        }
-        if(!is_zero) return 1;
+        return 1;
     }
     int total = 0;
     int temp_x = cur_x;
     int temp_y = cur_y;
+    int squares_temp = squares_left;
     for(int i = -1; i < 2; i++) {
         for(int j = -1; j < 2; j++) {
             if(i == 0 && j == 0) continue;
@@ -33,7 +27,9 @@ int countHSW_recurse(std::vector<std::vector<int>> board, int cur_x, int cur_y, 
                 cur_y = temp_y;
                 continue;
             }
+            squares_left--;
             total += countHSW_recurse(board, cur_x, cur_y, finish_x, finish_y, squares_left);
+            squares_left = squares_temp;
             cur_y = temp_y;
             cur_x = temp_x;
         }
