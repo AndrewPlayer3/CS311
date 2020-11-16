87cfff9ca8003c0a70192c4c02664aa41cd34fc0
Andrew Player <andrewplayer3@gmail.com>
2020-11-16 17:21:11 -0600

    more outline stuff

--- Project7/treesort.h
+++ Project7/treesort.h
@@ -12,18 +12,21 @@
 
 template<typename T>
 class BSTree {
+// Member Types:
+public:
+    typedef T value_type;
+
 // Member Variables
 private:
     value_type _data;
     std::unique_ptr<BSTree<value_type>> _left;
-    std::unique_ptr<BSTree<value_type>> _right;>
+    std::unique_ptr<BSTree<value_type>> _right;
     
-// Member Types:
-public:
-    typedef value_type T
-
 // Constructors and Destructors
 public:
+    // Default Constructor
+    // Guarantee: Strong
+    // Preconditions: None
     BSTree() {
         _data  = 0;
         _left  = nullptr;
@@ -32,10 +35,37 @@ public:
 
 // Member Functions
 public:
+    // Insert data into the proper location in the tree
+    // Guarantee: Strong
+    // Preconditions: None
+    std::unique_ptr<BSTree<value_type>> insert(
+        std::unique_ptr<BSTree<value_type>> node,
+        value_type data
+    );
+
+    // In-Order Traversal of the Binary Tree
+    // Guarantee: Strong
+    // Preconditions: None
+    void traverse();
 
 };
 
+// Stable sort that sorts into the memory of the original tree
+// Guarantee: Strong
+// Preconditions: None
 template<typename FDIter>
 void treesort(FDIter first, FDIter last);
+
+template<typename value_type>
+std::unique_ptr<BSTree<value_type>> BSTree<value_type>::insert(
+    std::unique_ptr<BSTree<value_type>> node,
+    value_type data
+) {
+    return node;
+}
+
+void traverse() {
+    return;
+}
 
 #endif
